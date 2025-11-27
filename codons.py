2604df2719a61386d5468c16e5118761cb277f69
LayanDabour <Layandabour308@gmail.com>
2025-11-27 23:56:50 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,12 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  codon_to_ameno={}
+  with open(file_path, "r") as file:
+    rows = file.readlines()
+  for row in rows[:]:
+    cell = row.strip().split('\t')
+    codon = cell[0]
+    ameno = cell[2]
+    codon_to_ameno[codon] = ameno
+  return codon_to_ameno  
 
 
